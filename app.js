9bce98430e5bdcfd76400ce3b6c200632aaf0082
Nicolo Veneziale <nicoloveneziale@gmail.com>
2025-03-28 11:42:11 +0000

    Config app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+require("dotenv").config();
+const router = require("./routes/routes");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
+
+app.use("/", router);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
